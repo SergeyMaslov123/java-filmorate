d03ba6f03cb9c16872b5f6b1a1718deee16c4e02
SergeyMaslov123 <122891606+SergeyMaslov123@users.noreply.github.com>
2023-07-24 23:00:51 +0300

    Revert "TZ 9 version 1.1"

--- src/main/java/ru/yandex/practicum/filmorate/model/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.model;
-
-public class ValidationException extends Exception {
-    public ValidationException (String massage) {
-        super(massage);
-    }
-}
