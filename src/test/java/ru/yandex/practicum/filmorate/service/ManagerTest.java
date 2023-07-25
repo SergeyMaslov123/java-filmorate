87711fbe267850b064d99b6825a30cf89d405a5f
Sergey Maslov <smaslov88@yandex.ru>
2023-07-25 23:04:36 +0300

    TZ 9 version 1.16

--- src/test/java/ru/yandex/practicum/filmorate/service/ManagerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/ManagerTest.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.model.ValidationException;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
