3784a8a39eb9c286eb96d3c4b2d97aab1b3e2c96
Sergey Maslov <smaslov88@yandex.ru>
2023-09-04 01:49:33 +0300

    TZ 11 version 1.2

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.dao.impl.GenresDaoImpl;
 
 @SpringBootApplication
 public class FilmorateApplication {
